63d900b5137351778cb81bc25fde9b8e0c4dca07
Michael Berg <mberg12@bu.edu>
2023-04-07 14:48:42 -0400

    variable exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declare and initialize variables
+            string name = "Tori";
+            int age = 31;
+            char lastInitial = 'W';
+            bool isTall = true;
+            double temperature = 93.6;
+            decimal futureTemperature = 76.75m;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"lastInitial: {lastInitial}");
+            Console.WriteLine($"isTall: {isTall}");
+            Console.WriteLine($"temperature: {temperature}");
+            Console.WriteLine($"futureTemperature: {futureTemperature}");
+            Console.WriteLine($"My name is {name}, My age is {age}, My last name begins with {lastInitial}, \n" +
+               $"Some people say I am {isTall}, The temperature right now is {temperature}, it will be {futureTemperature} later");
+
+
         }
     }
 }
